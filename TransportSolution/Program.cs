beea0a59d558ae90517be99393d7c7a9ce2dc277
Corey Sutton <coreysutton@outlook.co.nz>
2018-10-04 11:17:11 +1300

    Add base transport solution project

---
+++ TransportSolution/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreySutton.Xrm.Tooling.TransportSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

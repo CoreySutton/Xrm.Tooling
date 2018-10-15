45f9eb67024623fb5912715117f76e85f2452164
Corey Sutton <coreysutton@outlook.co.nz>
2018-10-16 10:42:09 +1300

    Add logging

--- CoreySutton.Xrm.Tooling.BulkExport/Program.cs
+++ CoreySutton.Xrm.Tooling.BulkExport/Program.cs
@@ -1,4 +1,7 @@
-﻿using CoreySutton.Xrm.Tooling.Core;
+﻿using System;
+using System.Collections.Generic;
+using CoreySutton.Utilities;
+using CoreySutton.Xrm.Tooling.Core;
 using CoreySutton.Xrm.Utilities;
 using Microsoft.Xrm.Sdk;
 
@@ -6,17 +9,45 @@ namespace CoreySutton.Xrm.Tooling.BulkExport
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        private static void Main()
         {
             string crmConnectionString = Properties.Settings.Default.CrmConnectionString;
             IOrganizationService organizationService = CrmConnectorUtil.Connect(crmConnectionString);
 
             Config config = ConfigParser<Config>.Read("Config.json");
+            IList<string> solutionUniqueNames = config.Solutions;
 
-            var solutionExport = new SolutionExport(organizationService);
-            foreach (string uniqueName in config.Solutions)
+            if (Validator.IsNullOrEmpty(solutionUniqueNames))
             {
-                solutionExport.ExportUnmanaged(uniqueName, $"{uniqueName}.zip");
+                ExConsole.WriteLine("No solutions found, exiting...");
+            }
+            else
+            {
+                ExConsole.WriteLine($"Discovered {solutionUniqueNames.Count} solutions");
+                ExportSolutions(solutionUniqueNames, organizationService);
+            }
+
+            ExConsole.WriteColor("Complete", ConsoleColor.Green);
+            Console.ReadLine();
+        }
+
+        private static void ExportSolutions(IList<string> solutionUniqueNames, IOrganizationService organizationService)
+        {
+            var solutionExport = new SolutionExport(organizationService);
+            foreach (string uniqueName in solutionUniqueNames)
+            {
+                ExConsole.Write($"Exporting {uniqueName}");
+                try
+                {
+                    solutionExport.ExportUnmanaged(uniqueName, $"{uniqueName}.zip");
+                    ExConsole.WriteLineToRight("[Done]");
+                }
+                catch (Exception ex)
+                {
+                    ExConsole.WriteLineToRight("[Failed]");
+                    ExConsole.WriteColor($"An exception occurred: {ex.Message}", ConsoleColor.Red);
+                    ExConsole.WriteColor(ex.StackTrace, ConsoleColor.DarkGray);
+                }
             }
         }
     }
